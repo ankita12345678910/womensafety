1672fb5a57f0d87f75fb6a15047f957cd924a9a5
Kumar Souvik Chatterjee <kumarsouvik.cse121042@bppimt.ac.in>
2025-04-24 22:21:22 +0530

    Updated Camera Table Location and Status

--- camerasapp/models.py
+++ camerasapp/models.py
@@ -7,10 +7,11 @@ class Camera(models.Model):
     owner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='cameras')
     name = models.CharField(max_length=255)
     ip_address = models.CharField(max_length=100,unique=True)
-    status=EnumField(choices=[('active','Active'),('inactive','Inactive'),('deleted','Deleted')])
+    status=EnumField(choices=[('active','Active'),('inactive','Inactive'),('deleted','Deleted')],default="active")
     added_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     viewers=models.ManyToManyField(User,related_name="viewable_cameras")
+    location=models.TextField(null=True)
     
     class Meta:
         db_table = "cameras"
